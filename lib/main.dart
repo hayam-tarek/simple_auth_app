9c495a7f3b0d954fffe0cb4a9752ad6958646110
hayam-tarek <hayamtareq28@gmail.com>
2024-11-02 00:07:27 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Simple Auth App',
       home: SignUpScreen(),
     );
   }
